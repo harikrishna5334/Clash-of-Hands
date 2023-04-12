b2f88364b9eda907bee1288d095b3242b1d3be5c
Harikrishna <hari.arush5334@gmail.com>
2023-04-12 13:57:15 +0530

    added build directory

--- build/rps.js
+++ build/rps.js
@@ -114,7 +114,7 @@ function playerGame(playerMove){
 
   document.querySelector('.js-result').innerHTML=result;
 
-  document.querySelector('.js-pickups').innerHTML=`You <img src="${playerMove}-emoji.png" alt="" class="move-css"> - <img src="${computerMove}-emoji.png" alt="" class="move-css"> Computer`;
+  document.querySelector('.js-pickups').innerHTML=`You <img src="/build/${playerMove}-emoji.png" alt="" class="move-css"> - <img src="/build/${computerMove}-emoji.png" alt="" class="move-css"> Computer`;
 
   // document.getElementById('res').innerHTML=result;
   
